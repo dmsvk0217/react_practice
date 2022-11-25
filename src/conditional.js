77254817296340b526503ce405d927f68ff6e2ca
dmsvk0217 <dmsvk0217@gmail.com>
2022-11-25 13:28:11 +0000

    element variable과 inline conditions

--- src/conditional.js
+++ src/conditional.js
@@ -2,24 +2,33 @@ import React, { useState } from "react";
 
 function Conditional() {
   const [isLogin, setisLogin] = useState(true);
+  const [count, setcount] = useState(1);
 
   const IsLoginhandler = function (a) {
     setisLogin(!isLogin);
     console.log("a : ", a);
   };
-  if (isLogin === true)
-    return (
-      <>
-        <div>환영합니다 고객님.</div>
-        <button onClick={IsLoginhandler}>버튼</button>
-      </>
-    );
-  return (
-    <>
-      <div>로그인이 필요합니다.</div>
-      <button onClick={() => setisLogin(!isLogin)}>버튼</button>
-    </>
+  const LoginComponent = (
+    <div>
+      {isLogin && <div>환영합니다 고객님.</div>}
+      <div>환영합니다 고객님.</div>
+      <button onClick={IsLoginhandler}>버튼</button>
+    </div>
   );
+  const LogoutComponent = (
+    <div>
+      {isLogin && <div>환영합니다 고객님.</div>}
+      <div>로그인이 필요합니다. 고객님.</div>
+      <button onClick={IsLoginhandler}>버튼</button>
+    </div>
+  );
+
+  return <div>{isLogin ? LoginComponent : LogoutComponent}</div>;
 }
 
 export default Conditional;
+
+/*
+conditional rendering 을 위한 element variable
+element variable을 간견하게 작성하기 위한 inline condition
+*/
